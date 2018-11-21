079c671066a6cfd840c613c5931dd1f586d9ed45
= <=>
2018-11-21 16:35:00 -0200

    controller concluidos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+
+
+  include WelcomeHelper
+  include LogsHelper
+
+=begin
   def usuario_autenticado?
     if session[:usuario_id].blank?
       redirect_to login_path, notice: "Efetue o login antes de entrar"
@@ -11,4 +17,10 @@ class ApplicationController < ActionController::Base
   def cuurent_usuario
     @current_usuario ||= Usuario.find_by_auth_token!(cookies[:auth_token])  if cookies[:auth_token]
   end
+
+=end
+
+
+
+
 end
--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,103 +1,42 @@
 class UsuariosController < ApplicationController
-  # GET /usuarios
-  # GET /usuarios.json
 
-  before_filter :usuario_autenticado?, :except => [:new,:create]
+  before_action :set_usuario, only: [:show, :edit, :update]
+
+  before_filter 'autenticado?'
 
   def index
     @usuarios = Usuario.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @usuarios }
-    end
   end
 
-  # GET /usuarios/1
-  # GET /usuarios/1.json
-  def show
-    @usuario = Usuario.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @usuario }
-    end
-  end
-
-  # GET /usuarios/new
-  # GET /usuarios/new.json
-  def new
-    @usuario = Usuario.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @usuario }
-    end
-  end
-
-  # GET /usuarios/1/edit
   def edit
-    @usuario = Usuario.find(params[:id])
   end
 
-  # POST /usuarios
-  # POST /usuarios.json
-  def create
-    @usuario = Usuario.new(params[:usuario])
-    @usuario.senha = Digest::SHA2.hexdigest("B10s$3G#{params[:usuario][:senha]}G3$s01B")
-    @usuario.senha_confirmation = Digest::SHA2.hexdigest("B10s$3G#{params[:usuario][:senha]}G3$s01B")
 
-    respond_to do |format|
-      if @usuario.save
-        format.html { redirect_to @usuario, notice: 'Usuario criado com sucesso.' }
-        format.json { render json: @usuario, status: :created, location: @usuario }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @usuario.errors, status: :unprocessable_entity }
-      end
-    end
+  def show
+
   end
-
-  # PUT /usuarios/1
-  # PUT /usuarios/1.json
-
-  # testar isso no console depois do almoço
 
   def update
-    @usuario = Usuario.find(params[:id])
-
+    addlog("Atualizando usuário")
     respond_to do |format|
-
-
-      if @usuario.update_attributes(params[:usuario])
-        format.html { redirect_to @usuario, notice: 'Usuario atualizado com sucesso.' }
-        format.json { head :ok }
+      if @usuario.update(usuario_params)
+        format.html { redirect_to usuarios_url, notice: 'Usuário alterado com sucesso.' }
       else
-        format.html { render action: "edit" }
+        format.html { render :edit }
         format.json { render json: @usuario.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /usuarios/1
-  # DELETE /usuarios/1.json
-  def destroy
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_usuario
     @usuario = Usuario.find(params[:id])
-    @usuario.destroy
-
-    respond_to do |format|
-      format.html { redirect_to usuarios_url }
-      format.json { head :ok }
-    end
   end
 
-
-=begin
-  def criptografa_senha(senhanova)
-    @usuario.senha = Digest::SHA2.hexdigest("B10s$3G#{senhanova}G3$s01B")
-    @usuario.save
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def usuario_params
+    params.require(:usuario).permit(:loginUsuario, :nomeUsuario, :tipoUsuario, :emailPrincipalUsuario, :emailAlternativoUsuario, :emailUspUsuario, :numeroTelefoneFormatado, :ramalUsp)
   end
-=end
-
 
 end
