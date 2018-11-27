06920a9039f674a8e62385a28c623c14c49c497a
= <=>
2018-11-27 15:59:20 -0200

    menus aparecendo em todas as telas

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,14 +1,14 @@
 class LogsController < ApplicationController
 
-  before_action :set_log, only: [:show]
 
-  before_filter 'autenticado?'
+
+  #before_filter 'autenticado?'
 
 
   # GET /logs
   # GET /logs.json
   def index
-    @logs = Log.all.order('id desc')
+    @logs = Log.order('id desc')
   end
 
   # GET /logs/1
@@ -16,10 +16,4 @@ class LogsController < ApplicationController
   def show
     @log = Log.find(params[:id])
   end
-
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def log_params
-      params.require(:log).permit(:acao, :ip)
-    end
 end
